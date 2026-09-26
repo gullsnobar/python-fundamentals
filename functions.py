60eee0a180018289ed111033288466466af931a2
gullsnobar <abdulrehmanali7775@gmail.com>
2026-09-26 20:29:16 +0500

    functions are completed

---
+++ functions.py
@@ -0,0 +1,84 @@
+# Python Functions
+# A function is a reusable block of code that performs a specific task.
+
+def greet(name):
+    print(f"Hello {name}")
+
+greet("Gull")    
+greet("Ali")
+
+# The main idea is write a logic at once and use it whenever you needed it.
+
+
+# Parameters allow a function to receive data.
+# In above function example name is paremeter and Gull is the argument.
+
+# Parameter = variable defined in the function
+# Argument = actual value passed to the function
+
+
+# return ---> This is one of the most imp concept to understand
+
+
+# Real world examples
+
+def send_welcome_email():
+    print("Sending welcome email.....")
+
+send_welcome_email()
+
+
+# Multiple Parameters
+# You can pass multiple values
+
+def calculate_total(price, quantity):
+    return price * quantity
+
+total = calculate_total(500, 3)
+print(total)
+
+
+# another example
+
+def create_user(name, email, age):
+    print(f"Name: {name}")
+    print(f"Email:{email}")
+    print(f"Age: {age}")
+
+create_user(
+    "Gull",
+    "gullsnobar09@gmail.com",
+    34
+)
+
+# return means Give this value back to whoever called me.
+# This is why return is heavily used in real applications.
+
+def add(a, b):
+    return a + b
+
+result = add(10,20)
+print(result)
+
+
+# A function can return different datatypes
+
+# List
+
+def get_users():
+    return ["Gull", "Snobar", "kamran"]
+
+users = get_users()
+print(users)
+
+# Dictionary
+
+def get_user():
+    return {
+        "id": 1,
+        "name": "Gulll",
+        "email": "gullsnobar08@gmail.com"
+    }
+
+user = get_user()  
+print(user["name"])  
